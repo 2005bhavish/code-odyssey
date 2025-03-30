256f6d10f47d4f7e69a1a842fea3961b359f6cd7
Bhavish Ankam <148208373+2005bhavish@users.noreply.github.com>
2025-03-30 09:41:16 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Code Odessey-vs',
+  title: 'Code Odessey',
   description: 'Made Up',
   generator: 'v0.dev',
   icons: { icon: '/favicon.ico' },
