9ed56342f827b0ec449bca584d3e03f0bbc2bf2c
Bhavish Ankam <148208373+2005bhavish@users.noreply.github.com>
2025-03-30 09:40:17 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,20 +2,20 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Code Odessey',
+  title: 'Code Odessey-vs',
   description: 'Made Up',
   generator: 'v0.dev',
-  icons: '/favicon.ico',
+  icons: { icon: '/favicon.ico' },
 }
-
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+      </head>
       <body>{children}</body>
     </html>
   )
